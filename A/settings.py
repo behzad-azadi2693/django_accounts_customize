435e8f54b81cf80bbd473f9e05959ea711cd9be9
joe <joelibral2693@gmail.com>
2021-03-21 16:45:43 +0330

    create app account and customizing User model,admin panel,forms.py

--- A/settings.py
+++ A/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'A.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'complete',
+        'USER': 'joe',
+        'PASSWORD': 'jl2693',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+#user costumize
+AUTH_USER_MODEL = 'accounts.User'
