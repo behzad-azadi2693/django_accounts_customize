4ba3f11edfc0a326d9f4dacf8ec87c585d00fc7a
joe <joelibral2693@gmail.com>
2021-03-22 02:03:13 +0430

    create views,url,template,forms for user login,logout,register

--- accounts/forms.py
+++ accounts/forms.py
@@ -32,3 +32,29 @@ class UserChangeForm(forms.ModelForm):
 
     def clean_password(self):
         return self.initial['password']
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
+class UserRegisterForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    def clean_user(self):
+        user = User.objects.get(username=self.username)
+        if user is not None:
+            raise ValueError('username existed of before')
+
+    def clean_phone_number(self):
+        user = User.objects.get(phone_number=self.phone_number)
+        if user is not None:
+            raise ValueError('tthis phone number is Repetitious')
+
+    def clean_phone_number(self):
+        user = User.objects.get(email=self.email)
+        if user is not None:
+            raise ValueError('tthis email is existed')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .models import User
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.changed_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request , 'uo loging', 'success')
+                return redirect('shop:home')
+            else:
+                messages.error(request, 'password or username denaid', 'danger')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form':form})
+
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'],cd['password'],cd['phone_number'],cd['email'])
+            user.save()
+            messages.success(request, 'you are register', 'success')
+            user_inc = authenticate(request, username=cd['username'], password=cd['password'])
+            login(request, user_inc)
+            return redirect('shop:home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form':form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'you are logout', 'success')
+    return redirect('shop:home')
